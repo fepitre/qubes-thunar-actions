5b181917779eabb75cc7903a8c6592df13706bad
Frédéric Pierret <frederic.epitre@orange.fr>
2017-11-07 06:55:12 -0500

    Update actions and handle filenames with spaces -

--- qvm-actions.sh
+++ qvm-actions.sh
@@ -1,44 +1,47 @@
 #!/bin/bash
 
+# Allow us to handle filenames with spaces using newline separator from Thunar
+IFS='
+'
+
 # Check if at least two arguments are provided: actions + file(s)
 if [ "$#" -le 1 ]; then
     echo "Not enough arguments provided. Aborting..."
 fi
 
 # File(s)
-files=$(echo $* | sed s/"$1 "//g)
+files=${*:2}
 
 # copy and move handle a list of files where other actions don't
 case $1 in
     copy)
-        /usr/lib/qubes/qvm-copy-to-vm.gnome "$files"
+        qvm-copy-to-vm '$default' $files | zenity --notification --text="Copying files..." --timeout 3
         ;;
     move)
-        /usr/lib/qubes/qvm-move-to-vm.gnome "$files"
+        qvm-move-to-vm '$default' $files | zenity --notification --text="Moving files..." --timeout 3
         ;;
     img)
         for file in $files
         do
-            /usr/lib/qubes/qvm-convert-img.gnome  "$file"
+            qvm-convert-img $file | zenity --notification --text="Converting file $file..." --timeout 3
         done
         ;;
     pdf)
         for file in $files
         do
-            /usr/lib/qubes/qvm-convert-pdf.gnome  "$file"
+            qvm-convert-pdf $file | zenity --notification --text="Converting file $file..." --timeout 3
         done
         ;;
     openvm)
-        vm=$(zenity --entry --text "Enter the destination VM name:")
         for file in $files
         do
-            qvm-open-in-vm $vm "$file" | zenity --notification --text "Opening $file in VM $vm..." --timeout 3 &
+            qvm-open-in-vm '$default' $file | zenity --notification --text "Opening $file in VM..." --timeout 3 &
         done
         ;;
     opendvm)
         for file in $files
         do
-            qvm-open-in-dvm "$files" | zenity --notification --text "Opening $file in DisposableVM..." --timeout 3 &
+            qvm-open-in-dvm $files | zenity --notification --text "Opening $file in DisposableVM..." --timeout 3 &
         done
         ;;
     *)
